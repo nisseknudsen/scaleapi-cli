fa967109ee4803ea3d2b51219d3283dfb8b5d2bd
nisseknudsen <nisse.knudsen@gmail.com>
2020-08-09 00:26:37 +0200

    Working add functionality, list

--- src/main.rs
+++ src/main.rs
@@ -1,153 +1,214 @@
+use clap::{App, Arg};
 use ini::Ini;
-use ini::ini::Properties;
 use shellexpand;
+use std::collections::HashMap;
+use std::fmt::Debug;
 use std::fs;
-use std::borrow::Cow;
 use std::os::unix::fs::PermissionsExt;
+use std::process;
 use std::string::String;
-use structopt::StructOpt;
 
-#[derive(Debug, StructOpt)]
-#[structopt(name = "Cli", about = "Arguments for Scale CLI")]
-struct Cli {
-    command_type: String,
-
-    //#[structopt(required_if("command_type", "list"))]
-    account_name: Option<String>,
-
-    //#[structopt(required_if("account_name", Some)]
-    key_name: Option<String>,
-    /*
-    /// Activate debug mode
-    // short and long flags (-d, --debug) will be deduced from the field's name
-    #[structopt(short, long)]
-    debug: bool,
-
-    /// Set speed
-    // we don't want to name it "speed", need to look smart
-    #[structopt(short = "v", long = "velocity", default_value = "42")]
-    speed: f64,
-
-    /// Input file
-    #[structopt(parse(from_os_str))]
-    input: PathBuf,
-
-    /// Output file, stdout if not present
-    #[structopt(parse(from_os_str))]
-    output: Option<PathBuf>,
-
-    /// Where to write the output: to `stdout` or `file`
-    #[structopt(short)]
-    out_type: String,
-
-    /// File name: only required when `out-type` is set to `file`
-    #[structopt(name = "FILE", required_if("out-type", "file"))]
-    file_name: Option<String>,
-    */
-}
-
+#[derive(Debug)]
 struct ScaleCredentials {
     filepath: String,
-    accounts: Vec<ScaleAccount>
+    accounts: HashMap<String, ScaleAccount>,
 }
 
+#[derive(Debug)]
 struct ScaleAccount {
     name: String,
-    keys: Vec<ScaleApiKey>
+    keys: HashMap<String, ScaleApiKey>,
 }
 
+#[derive(Debug)]
 struct ScaleApiKey {
     name: String,
-    value: String
+    value: String,
 }
 
 impl ScaleCredentials {
-    pub fn new(fp: String) -> ScaleCredentials {
-        ScaleCredentials {
-            filepath: fp,
-            accounts: vec![]
-        }
+    pub fn new(fp: &String) -> ScaleCredentials {
+        let credentials = ScaleCredentials {
+            filepath: fp.clone(),
+            accounts: HashMap::new(),
+        };
+
+        return credentials;
     }
 
-    pub fn from_ini(ini: Ini, fp: String) -> ScaleCredentials {
-        let mut credentials: ScaleCredentials = ScaleCredentials {
-            filepath: fp,
-            accounts: vec![]
+    pub fn from_file(fp: &String) -> ScaleCredentials {
+        match Ini::load_from_file(&fp) {
+            Ok(credentials) => return ScaleCredentials::from_ini(credentials, fp),
+            Err(_error) => {
+                println!("Not able to parse file. Creating empty credentials config.");
+                return ScaleCredentials::new(fp);
+            }
         };
-        
-        for k in ini.sections() {
-            match k {
-                Some(val) => {
-                    credentials.accounts.push(
-                        ScaleAccount{
-                            name: String::from(val),
-                            keys: vec![]
+    }
+
+    pub fn add_entry(mut self, account: &String, label: &String, value: &String) -> Self {
+        let mut account_entry: ScaleAccount = match self.accounts.remove(account) {
+            Some(k) => k,
+            None => ScaleAccount {
+                name: String::from(account),
+                keys: HashMap::new(),
+            },
+        };
+
+        let key_entry: ScaleApiKey = match account_entry.keys.remove(label) {
+            Some(_) | None => ScaleApiKey {
+                name: String::from(label),
+                value: String::from(value),
+            },
+        };
+
+        account_entry.keys.insert(label.clone(), key_entry);
+        self.accounts.insert(account.clone(), account_entry);
+
+        return self;
+    }
+
+    pub fn save(&self) {
+        let credentials_file: &String = &self.filepath;
+        let ini: Ini = self.to_ini();
+        ini.write_to_file(credentials_file).unwrap();
+        fs::set_permissions(credentials_file, fs::Permissions::from_mode(0o600)).unwrap();
+    }
+
+    fn from_ini(ini: Ini, fp: &String) -> ScaleCredentials {
+        let mut credentials: ScaleCredentials = ScaleCredentials::new(fp);
+
+        for j in ini.sections() {
+            match j {
+                Some(s_val) => {
+                    let mut account = ScaleAccount {
+                        name: String::from(s_val),
+                        keys: HashMap::new(),
+                    };
+
+                    match ini.section(Some(&account.name)) {
+                        Some(properties) => {
+                            for (k, v) in properties.iter() {
+                                let api_key = ScaleApiKey {
+                                    name: String::from(k),
+                                    value: String::from(v),
+                                };
+                                account.keys.insert(api_key.name.clone(), api_key);
+                            }
                         }
-                    );
-                },
-                None => continue
+                        None => println!("Not able to parse content for section {}", &account.name),
+                    }
+
+                    credentials.accounts.insert(account.name.clone(), account);
+                }
+                None => {}
             }
         }
 
-        return credentials
+        return credentials;
+    }
+
+    fn to_ini(&self) -> Ini {
+        let mut ini: Ini = Ini::new();
+        for (_ak, av) in &self.accounts {
+            for (_kk, kv) in &av.keys {
+                ini.set_to(Some(&av.name), kv.name.clone(), kv.value.clone());
+            }
+        }
+
+        return ini;
     }
 }
 
 fn main() {
-    println!("Hello, init!");
+    let app = App::new("Scale CLI")
+        .version("0.1")
+        .author("Nisse Knudsen <nisse.knudsen@gmail.com>")
+        .about("Manage Scale API Keys via CLI")
+        .arg(
+            Arg::with_name("command")
+                .help("Sets the command to execute")
+                .index(1)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("account")
+                .help("Sets the account name to apply operation on")
+                .index(2)
+                .required_ifs(&[("command", "add")]),
+        )
+        .arg(
+            Arg::with_name("label")
+                .help("Label of API Key")
+                .index(3)
+                .required_ifs(&[("command", "add")]),
+        )
+        .arg(
+            Arg::with_name("value")
+                .help("Value of API Key")
+                .index(4)
+                .required_ifs(&[("command", "add")]),
+        );
 
-    
-    let mut credentials: Ini = load_or_create_credentials(&credentials_file);
+    let matches = app.get_matches();
 
-    let args = Cli::from_args();
-    println!("{:?}", args);
+    match matches.value_of("command") {
+        Some(command) => {
+            let credentials: ScaleCredentials = load_or_create_credentials();
+            match command {
+                // list details from credentials file
+                "list" => match matches.value_of("account") {
+                    // If account is provided as next argument, print all stored keys for that account
+                    Some(account) => match credentials.accounts.get(account) {
+                        Some(a) => {
+                            for (_kk, kv) in &a.keys {
+                                println!("{}\t{}", kv.name, kv.value);
+                            }
+                        }
+                        None => {}
+                    },
+                    // If no account has been provided, just list all accounts available for selection
+                    None => {
+                        for (_ak, av) in credentials.accounts {
+                            println!("{}", av.name);
+                        }
+                    }
+                },
+                "add" => {
+                    let account: String = match matches.value_of("account") {
+                        Some(k) => String::from(k),
+                        None => process::exit(1), // should not be reached because argument is required
+                    };
 
-    if args.command_type == "list" {
-        if args.account_name == None {
-            for k in credentials.sections() {
-                println!("{}", k.unwrap_or("No sections available."));
-            }
-        } else {
-            let section_name: Option<String> = Some(args.account_name.unwrap());
-            let section: &Properties = credentials.section(section_name).unwrap();
+                    let label: String = match matches.value_of("label") {
+                        Some(k) => String::from(k),
+                        None => process::exit(1), // should not be reached because argument is required
+                    };
 
-            if args.key_name == None {
-                for (key, value) in section.iter() {
-                    println!("{}\t{}", key, value);
+                    let value: String = match matches.value_of("value") {
+                        Some(a) => String::from(a),
+                        None => process::exit(1), // should not be reached because argument is required
+                    };
+
+                    credentials.add_entry(&account, &label, &value).save();
                 }
-            } else {
-                let property_name = args.key_name.unwrap();
-                println!("{}", section.get(property_name).unwrap_or(""))
+                _ => {}
             }
         }
-    } else if args.command_type == "add" {
+        None => {}
     }
-
-    credentials
-        .with_section(Some("default"))
-        .set("API_KEY", "32423423123");
-    save_credentials(&credentials, &credentials_file);
-}
-
-fn save_credentials(credentials: &Ini, credentials_file: &String) {
-    credentials.write_to_file(credentials_file).unwrap();
-    fs::set_permissions(credentials_file, fs::Permissions::from_mode(0o600)).unwrap();
 }
 
 fn get_directory() -> String {
-    let home_dir: Cow<str> = shellexpand::tilde("~");
-    let scaleapi_dir: String = format!("{home_dir}/.scaleapi", home_dir = home_dir);
-    fs::create_dir_all(&scaleapi_dir).unwrap();
-
-    return scaleapi_dir;
+    let scaleapi_dir: String = format!("{home_dir}/.scaleapi", home_dir = shellexpand::tilde("~"));
+    match fs::create_dir_all(&scaleapi_dir) {
+        Ok(_) => return scaleapi_dir,
+        Err(error) => panic!(error),
+    };
 }
 
 fn load_or_create_credentials() -> ScaleCredentials {
-    let credentials_file: String = format!("{scaleapi_dir}/credentials", scaleapi_dir = get_directory());
-
-
-    let credentials = match Ini::load_from_file(credentials_file) {
-        Ok(credentials) => ScaleCredentials::from_ini(credentials, credentials_file),
-        Err(_error) => ScaleCredentials::new(credentials_file),
-    };
+    let credentials_file: String =
+        format!("{scaleapi_dir}/credentials", scaleapi_dir = get_directory());
+    return ScaleCredentials::from_file(&credentials_file);
 }
